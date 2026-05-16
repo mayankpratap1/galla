299afe850143aa67da8b3d8da7fab63bfe87ebe6
mayank <pratapmayank10@gmail.com>
2026-05-16 23:42:44 +0000

    Build config: fix plugin setup, use annotationProcessor instead of KSP

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,16 +1,6 @@
-pluginManagement {
-    repositories {
-        google()
-        mavenCentral()
-        gradlePluginPortal()
-    }
-}
-
 plugins {
     id("com.android.application") version "8.9.1" apply false
     id("org.jetbrains.kotlin.android") version "2.3.0" apply false
     id("org.jetbrains.kotlin.plugin.compose") version "2.3.0" apply false
     id("org.jetbrains.kotlin.plugin.serialization") version "2.3.0" apply false
-    id("com.google.devtools.ksp") version "2.3.0" apply false
-    id("com.google.dagger.hilt.android") version "2.52" apply false
 }
