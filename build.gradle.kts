feae89f9b21273f23a495a33f27749ec0bb40006
mayankpratap1 <pratapmayank10@gmail.com>
2026-05-16 21:49:05 +0000

    Fix: Kotlin 2.0.21 + KSP 2.0.0-1.0.25

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,9 +7,9 @@ buildscript {
 
 plugins {
     id("com.android.application") version "8.9.1" apply false
-    id("org.jetbrains.kotlin.android") version "2.1.0" apply false
-    id("org.jetbrains.kotlin.plugin.compose") version "2.1.0" apply false
-    id("org.jetbrains.kotlin.plugin.serialization") version "2.1.0" apply false
-    id("com.google.devtools.ksp") version "2.1.0-1.0.22" apply false
+    id("org.jetbrains.kotlin.android") version "2.0.21" apply false
+    id("org.jetbrains.kotlin.plugin.compose") version "2.0.21" apply false
+    id("org.jetbrains.kotlin.plugin.serialization") version "2.0.21" apply false
+    id("com.google.devtools.ksp") version "2.0.0-1.0.25" apply false
     id("com.google.dagger.hilt.android") version "2.51.1" apply false
 }
