16cba64e557ad1bd0adabcb78775c49479cee688
mayankpratap1 <pratapmayank10@gmail.com>
2026-05-16 22:18:34 +0000

    Fix: Kotlin 2.3.0 + Hilt 2.52 to match libraries
    llamacpp-kotlin 0.4.0 and litertlm require Kotlin 2.3+ metadata

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,8 +10,8 @@ buildscript {
 
 plugins {
     id("com.android.application") version "8.9.1" apply false
-    id("org.jetbrains.kotlin.android") version "2.0.0" apply false
-    id("org.jetbrains.kotlin.plugin.compose") version "2.0.0" apply false
-    id("org.jetbrains.kotlin.plugin.serialization") version "2.0.0" apply false
-    id("com.google.dagger.hilt.android") version "2.51.1" apply false
+    id("org.jetbrains.kotlin.android") version "2.3.0" apply false
+    id("org.jetbrains.kotlin.plugin.compose") version "2.3.0" apply false
+    id("org.jetbrains.kotlin.plugin.serialization") version "2.3.0" apply false
+    id("com.google.dagger.hilt.android") version "2.52" apply false
 }
