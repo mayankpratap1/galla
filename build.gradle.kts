1c5f6a4fc0fb9822156e287e53719587488c2d6a
mayankpratap1 <pratapmayank10@gmail.com>
2026-05-16 22:30:40 +0000

    Fix: Update KSP to 2.0.0-1.0.25 for Kotlin 2.3

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,7 +4,7 @@ buildscript {
         mavenCentral()
     }
     dependencies {
-        classpath("com.google.devtools.ksp:com.google.devtools.ksp.gradle.plugin:2.0.0-1.0.21")
+        classpath("com.google.devtools.ksp:com.google.devtools.ksp.gradle.plugin:2.0.0-1.0.25")
     }
 }
 
